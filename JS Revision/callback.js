5e733dedac8bf4d81bbd665664eb63673b7cddb7
Srishti Srivastava <ssrishti508@gmail.com>
2023-08-10 22:10:03 +0530

    callbacks

---
+++ JS Revision/callback.js
@@ -0,0 +1,6 @@
+function higherOrderFunction(callback){
+    callback();
+}
+
+//A function passed as an argument to another function is called callback function
+//the function accepting that argumnet is called higher order function
