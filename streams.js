ca2144e47b2a20e7efdcc383ff4fe03df691eada
Srishti Srivastava <ssrishti508@gmail.com>
2023-09-09 10:08:44 +0530

    pipes and chaining

--- streams.js
+++ streams.js
@@ -6,14 +6,29 @@
 //stream is infact a built in module that inherits from the event emitter class.
 
 
+//consider pipe same as a water pipe. reading water from the tank and writing it in the bucket
+
+
 //const { read } = require("node:fs");
 const fs = require("node:fs")
+const zlib = require("node:zlib") //allows you to create zipped files
+
+const gzip = zlib.createGzip();
 
 const readableStream = fs.createReadStream("./file.txt",{encoding:"utf-8",highWaterMark:2});
 
 const writeableStream = fs.createWriteStream("./file1.txt");
 
-readableStream.on("data",(chunk)=>{
-    console.log(chunk);
-    writeableStream.write(chunk);
-})
+// readableStream.on("data",(chunk)=>{
+//     console.log(chunk);
+//     writeableStream.write(chunk);
+// })
+
+
+//pipes
+
+//readableStream.pipe(writeableStream);
+
+//chaining pipes
+//creating zipped version of file and writing it in file1 
+readableStream.pipe(gzip).pipe(fs.WriteStream("./file1.txt.gz"));
